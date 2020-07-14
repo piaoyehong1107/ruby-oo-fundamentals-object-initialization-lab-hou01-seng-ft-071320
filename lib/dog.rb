ac225b2dbcfe8ac9c1c7fbf8bcbd88e7d1fa76a5
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-14 19:05:06 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def initialize (dog_name,breed="Mutt")
+    @name = dog_name
+    @breed = breed
+  end
+  def name=(name)
+    @name = dog_name
+  end
+   def breed=(breed)
+    @breed = breed
+  end
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end
+end 
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person 
+  def initialize (person_name)
+    @name = person_name
+  end
+  def person_name=(person_name)
+    @name=person_name
+  end
+  def person_name
+    @name
+  end  
+end 
