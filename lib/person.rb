9c3cd7f3e8a95d0d0d5aed84b3c05178052785e8
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-14 18:56:27 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person 
+  def initi (person_name)
+    @person_name = person_name
+  end
+  def person_name=(person_name)
+    @person_name=person_name
+  end
+  def person-name
+    @person_name
+  end  
+end 
